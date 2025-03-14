53f4bf270b4366e81c1cf34ed7f1ea5c7d9c8b3d
spydermurph <69465629+spydermurph@users.noreply.github.com>
2025-03-14 11:36:55 +0000

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,28 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<StoreContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+
+DbInitializer.InitDb(app);
+
+app.Run();
