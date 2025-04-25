12d38be72578d445c7ee3a598d656eaefa459dd0
spydermurph <69465629+spydermurph@users.noreply.github.com>
2025-04-25 13:25:18 +0000

    Inventory complete

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.RequestHelpers;
 using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -8,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("Cloudinary"));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -18,8 +19,10 @@ builder.Services.AddDbContext<StoreContext>(options =>
 });
 
 builder.Services.AddCors();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddTransient<ExceptionMiddleware>();
 builder.Services.AddScoped<PaymentsService>();
+builder.Services.AddScoped<ImageService>();
 builder.Services.AddIdentityApiEndpoints<User>(option =>
 {
     option.SignIn.RequireConfirmedAccount = false;
---
+++ client/src/features/admin/adminApi.ts
@@ -0,0 +1,41 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
+import { Product } from "../../app/models/product";
+
+export const adminApi = createApi({
+  reducerPath: "adminPath",
+  baseQuery: baseQueryWithErrorHandling,
+  endpoints: (builder) => ({
+    createProduct: builder.mutation<Product, FormData>({
+      query: (data: FormData) => {
+        return {
+          url: "products",
+          method: "POST",
+          body: data,
+        };
+      },
+    }),
+    updateProduct: builder.mutation<void, { id: number; data: FormData }>({
+      query: ({ id, data }) => {
+        data.append("id", id.toString());
+        return {
+          url: "products",
+          method: "PUT",
+          body: data,
+        };
+      },
+    }),
+    deleteProduct: builder.mutation<void, number>({
+      query: (id: number) => ({
+        url: `products/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+} = adminApi;
